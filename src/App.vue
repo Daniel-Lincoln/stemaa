aa1c447adafee6e84db32b45ce1e98ea01cd61d1
Ermiyas Misganaw <ermimisge@gmail.com>
2024-04-19 11:08:35 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Navigation from "@/components/Navigation.vue";
+import HomeView from "./components/HomeView.vue";
+import sponser from "@/components/sponser.vue";
+import TimeLine from "./components/TimeLine.vue";
+import Alumini from "./components/Alumini.vue";
+import footerss from "./components/footerss.vue";
+import { RouterLink, RouterView } from 'vue-router'
+import GalleryVue from "./components/Gallery.vue";
+
+</script>
+
+<template>
+  <Navigation />
+  <HomeView/>
+  
+  <TimeLine/>
+  <GalleryVue/>
+  <sponser/>
+  <alumini/>
+  <footerss/>
+</template>
+<style >
+
+
+</style>
---
+++ src/components/sponser.vue
@@ -0,0 +1,39 @@
+<template>
+      <h1 class="h1" style="font-family: Calibri">Sponsors</h1>
+<div class="sponsor-container">
+    <img src="src/assets/images/Ethiopianairlineslogo.png" alt="Snow"  >
+    <img src="../assets/images/boing.png" alt="Forest" >
+    <img src="../assets/images/thinkyounglogo.jpg" alt="Mountains" >
+</div>
+
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.sponsor-container  {
+     display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: whitesmoke;
+
+  }
+img{
+    width:150px;
+    margin-right: 190px;   /* Horizontal Space */
+}
+h1{
+  text-align: center;
+  margin-top: 0%;
+  font-family: 'Times New Roman', Times, serif;
+  font-weight: bolder;
+
+}
+
+</style>
